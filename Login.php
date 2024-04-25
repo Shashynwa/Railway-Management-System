7eb62be8d1baf75323e0b3b61b2546fcbed509c1
kaneki-212903 <132456512+kaneki-212903@users.noreply.github.com>
2024-04-26 01:36:38 +0530

    Login.php added in the repo

---
+++ Login.php
@@ -0,0 +1,43 @@
+<?php
+session_start();
+
+$servername = "localhost";
+$Username = "root";
+$dbPassword = "";
+$dbname = "railway management";
+
+// Connect to the database
+$conn = new mysqli($servername, $Username, $dbPassword, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Login process
+if (isset($_POST['username']) && isset($_POST['password'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    
+    $username = stripslashes($username);
+    $password = stripslashes($password);
+    $username = mysqli_real_escape_string($conn, $username);
+    $password = mysqli_real_escape_string($conn, $password);
+
+    
+    $query = "SELECT * FROM user WHERE (username='$username' OR email='$username') AND password='$password'";
+    $result = mysqli_query($conn, $query);
+    $count = mysqli_num_rows($result);
+
+    if ($count == 1) {
+        $_SESSION['username'] = $username;
+        // Return a JSON response instead of redirecting
+        echo json_encode(['status' => 'success', 'message' => 'Login successful']);
+    } else {
+        echo json_encode(['status' => 'error', 'message' => 'Invalid username or password']);
+    }
+}
+
+$conn->close();
+?>
